bd92ae51e75ff26520f8ff341fd7db664eadad74
Dhruv Patel <dhruvp.dev@gmail.com>
2025-03-28 10:04:29 +0530

    add typing

--- chat.js
+++ chat.js
@@ -1,4 +1,3 @@
-
 const socket = io('https://chat-app-backend-api-88av.onrender.com');
 socket.on('connect', () => console.log('✅ WebSocket connected:', socket.id));
 socket.on('connect_error', (err) => console.error('❌ WebSocket error:', err.message));
@@ -7,28 +6,28 @@ const userId = localStorage.getItem('userId');
 socket.emit('login', userId);
 
 const chatBox = document.getElementById('chatBox');
-
+socket.on('stopTyping', stopTyping);
+socket.on('typing', addTyping);
 socket.on('sendMsg', (msg) => {
     const div = document.createElement('div');
-    div.classList.add('message','received' );
+    div.classList.add('message', 'received');
     div.textContent = msg;
     chatBox.appendChild(div);
 });
 
 // const username = document.getElementById('username')
-// username.innerText = 
+// username.innerText =
 const queryParams = new URLSearchParams(window.location.search);
 const receiverId = queryParams.get('userId');
 
-
 async function loadChat() {
     const token = localStorage.getItem('token');
-  
+
     if (!token || !receiverId) {
         window.location.href = 'index.html'; // Redirect if no token or user ID
         return;
     }
-   
+
     // Fetch user details (name)
     const userResponse = await fetch(`https://chat-app-backend-api-88av.onrender.com/api/v1/user/get/${receiverId}`, {
         headers: { Authorization: token },
@@ -50,6 +49,8 @@ async function loadChat() {
 }
 
 async function sendMessage() {
+    stopTyping();
+    sendStopTyping()
     const token = localStorage.getItem('token');
     const messageInput = document.getElementById('messageInput');
     const content = messageInput.value.trim();
@@ -83,5 +84,28 @@ function addMsg(msg) {
     div.textContent = msg.content;
     chatBox.appendChild(div);
 }
+function addTyping() {
+    if (!document.getElementById('typing')) {
+        setTimeout(() => {
+            const div = document.createElement('div');
+            div.classList.add('received', 'typing');
+            div.id = 'typing';
+            div.textContent = 'typing...';
+            chatBox.appendChild(div);
+        }, 0);
+    }
+}
 
-window.onload=loadChat(); // Load chat on page load
+function stopTyping() {
+    const typing = document.getElementById('typing');
+    if(typing)chatBox.removeChild(typing);
+}
+
+function sendTyping() {
+    socket.emit('typing', receiverId);
+}
+
+function sendStopTyping(){
+   socket.emit('stopTyping',receiverId) 
+}
+window.onload = loadChat(); // Load chat on page load
